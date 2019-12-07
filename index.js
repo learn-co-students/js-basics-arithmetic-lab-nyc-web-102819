d62e164a28128ce778ae2d7bab10c5960fad992f
jlindner22 <jenlindner22@gmail.com>
2019-12-07 17:09:47 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here
+let multiply = 0
+let var1 = 31
+let var2 = 2
+ multiply = (var1 * var2)
+
+let random = 0
+random = Math.round(Math.random() * 10) + 1
+
+let mod = 0
+mod = 10 % 6
+
+let max = 0
+max = Math.max(1, 20)
