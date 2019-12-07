046c503c7063977eeddf5bf81c126ff1ad6c0764
jlindner22 <jenlindner22@gmail.com>
2019-12-07 17:11:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-let multiply = 0
 let var1 = 31
 let var2 = 2
- multiply = (var1 * var2)
+let multiply = (var1 * var2)
 
-let random = 0
-random = Math.round(Math.random() * 10) + 1
+let random = Math.round(Math.random() * 10) + 1
 
-let mod = 0
-mod = 10 % 6
+let mod = 10 % 6
 
-let max = 0
-max = Math.max(1, 20)
+let max = Math.max(1, 20)
